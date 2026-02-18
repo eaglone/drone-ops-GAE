20823a08a1a5e4b2a8def78fdb75b27cabfce211
eaglone <bauduromain@gmail.com>
2026-02-18 10:05:42 +0100

    Update main.js

--- app/main.js
+++ app/main.js
@@ -185,3 +185,8 @@ function safeCall(fnName){
         }
     }
 }
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js")
+        .then(() => console.log("Service Worker: Registered"))
+        .catch(err => console.error("Service Worker: Error", err));
+}

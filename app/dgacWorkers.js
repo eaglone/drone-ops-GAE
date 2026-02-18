11aa2fe80d8072a54fa880c91ea2062791f85606
eaglone <bauduromain@gmail.com>
2026-02-18 11:07:50 +0100

    Update dgacWorkers.js

--- app/dgacWorkers.js
+++ app/dgacWorkers.js
@@ -1,12 +1,8 @@
-importScripts(
-    "https://unpkg.com/@turf/turf@6/turf.min.js"
-);
+importScripts("https://unpkg.com/@turf/turf@6/turf.min.js");
 
 self.onmessage = e => {
 
     const geojson = e.data;
-
-    console.log("Worker: simplification...");
 
     const simplified = turf.simplify(geojson, {
         tolerance: 0.0001,

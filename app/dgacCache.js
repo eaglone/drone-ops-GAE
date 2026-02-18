4d59c1d62bfbcb32e50d5d1ab88dd18f1eda07ad
eaglone <bauduromain@gmail.com>
2026-02-18 11:07:21 +0100

    Update dgacCache.js

--- app/dgacCache.js
+++ app/dgacCache.js
@@ -1,13 +1,15 @@
-const DB_NAME = "dgacCacheDB";
-const STORE = "zones";
+// DGAC CACHE GLOBAL
 
-function openDB() {
+const DGAC_DB_NAME = "dgacCacheDB";
+const DGAC_STORE = "zones";
+
+function openDGACDB() {
     return new Promise((resolve, reject) => {
 
-        const req = indexedDB.open(DB_NAME, 1);
+        const req = indexedDB.open(DGAC_DB_NAME, 1);
 
         req.onupgradeneeded = e => {
-            e.target.result.createObjectStore(STORE);
+            e.target.result.createObjectStore(DGAC_STORE);
         };
 
         req.onsuccess = e => resolve(e.target.result);
@@ -15,33 +17,32 @@ function openDB() {
     });
 }
 
-export async function saveDGAC(data) {
-    const db = await openDB();
+window.saveDGAC = async function(data) {
+    const db = await openDGACDB();
+    const tx = db.transaction(DGAC_STORE, "readwrite");
 
-    const tx = db.transaction(STORE, "readwrite");
-    tx.objectStore(STORE).put({
+    tx.objectStore(DGAC_STORE).put({
         timestamp: Date.now(),
         data
     }, "dgac");
-}
+};
 
-export async function loadDGAC() {
-    const db = await openDB();
+window.loadDGAC = async function() {
+    const db = await openDGACDB();
 
     return new Promise(resolve => {
-        const tx = db.transaction(STORE, "readonly");
-        const req = tx.objectStore(STORE).get("dgac");
+        const tx = db.transaction(DGAC_STORE, "readonly");
+        const req = tx.objectStore(DGAC_STORE).get("dgac");
 
         req.onsuccess = () => {
             const result = req.result;
 
             if (!result) return resolve(null);
 
-            // cache 24h
             if (Date.now() - result.timestamp > 86400000)
                 return resolve(null);
 
             resolve(result.data);
         };
     });
-}
+};
